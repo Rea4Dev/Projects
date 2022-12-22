660bc3c48dafdf2dff803a69ec3b6e248747d11a
Rea <Rea4Dev@outlook.com>
2022-12-22 19:53:18 -0300

    teste

---
+++ Initial/script.js
@@ -0,0 +1,24 @@
+function verificar(){
+        var largura = window.screen.width;
+        if (largura < 900){
+            window.document.body.style.backgroundColor="black";
+            window.alert('Por enquanto não projetei esta página para celulares. Entre por um PC ou aguarde eu adaptá-la.');
+            window.location.replace('https://rea4dev.github.io/Projects/Initial/ProjetosEX.html');
+        }
+}
+function verificar(){
+    var largura = window.screen.width;
+    if (largura < 900){
+        window.document.body.style.backgroundColor="black";
+        window.alert('Por enquanto não projetei esta página para celulares. Entre por um PC ou aguarde eu adaptá-la.');
+        window.location.replace('https://rea4dev.github.io/Projects/Initial/ProjetosEX.html');
+    }
+}
+function verificar(){
+    var largura = window.screen.width;
+    if (largura < 900){
+        window.document.body.style.backgroundColor="black";
+        window.alert('Por enquanto não projetei esta página para celulares. Entre por um PC ou aguarde eu adaptá-la.');
+        window.location.replace('https://rea4dev.github.io/Projects/Initial/ProjetosEX.html');
+    }
+}
